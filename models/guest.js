3f350e73586a42d7cb5c0fb9d10af62df17b81d6
pablackhawk <l.tirtarahardja@gmail.com>
2018-03-05 21:12:09 -0800

    Guest schema complete

---
+++ models/guest.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const guestSchema = new Schema({
+  firstName: {
+    type: String,
+    required: 'Please enter your first name',
+    trim: true
+  },
+  lastName: {
+    type: String,
+    required: 'Please enter your last name',
+    trim: true
+  },
+  phoneNumber: {
+    type: String,
+    required: 'Please enter your phone number',
+    trim: true,
+    match: [
+      '^([0-9]{3}-[0-9]{3}-[0-9]{4}$',
+      'Please enter a valid phone number'
+    ],
+    unique: true
+  },
+  partySize: {
+    type: Number,
+    required: 'Please enter your party size',
+    trim: true
+  },
+  moveUp: { type: Boolean, default: false },
+  moveDown: { type: Boolean, default: false },
+  seated: { type: Boolean, default: false },
+  date: { type: Date, default: Date.now },
+  email: {
+    type: String,
+    match: [/.+\@.+\..+/, 'Please enter a valid e-mail address']
+  }
+});
+
+const Guest = mongoose.model('Guest', guestSchema);
+
+module.exports = Guest;
