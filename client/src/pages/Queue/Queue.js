63e220b87be377c2e3a950832e0129304fb6dafe
pablackhawk <l.tirtarahardja@gmail.com>
2018-03-28 22:45:39 -0700

    fixed merge error

--- client/src/pages/Queue/Queue.js
+++ client/src/pages/Queue/Queue.js
@@ -1,22 +1,60 @@
 import React, { Component } from 'react';
 import API from '../../utils/API';
-
 import styled from 'styled-components';
 import NewReservation from '../../components/NewReservation';
-import Counter from "../../components/Counter";
-import Icons from "../../components/Icons";
-import CustomerQueue from "../../components/CustomerQueue";
+import Counter from '../../components/Counter';
+import Icons from '../../components/Icons';
+import CustomerQueue from '../../components/CustomerQueue';
 
-const Container = styled.div`
+const Wrapper = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: space-around;
+`;
+
+const Container1 = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+`;
+const Container2 = styled.div`
+  display: flex;
+`;
+
+/*const Input = styled.input`
+    color: #8FBC8B;
+    font-size: 18px;
+    background: #EBEDEF;
+    margin-bottom: 15px;
+    border: none;
+    width: 500px;
+    height: 30px;
+    margin-left: 10px;
+`;
+const Button = styled.button`
+    display: block;
+    align: center;
+    border-radius: 4px;
+    height: 30px;
+    background-color: #FF6347;
+    border: none;
+    color: #FFFFFF;
+    text-align: center;
+    font-size: 12px;
+    width: 100px;
+    transition: all 0.5s;
+    cursor: pointer;
+    margin-top: 20px;
+`;
+const List = styled.li`
+  font-weight: bold;
 `
-import { Col, Row } from '../../components/Grid';
-import { FormBtn } from '../../components/Form';
-import { Container, Input, Logo } from '../../components/Styled/Styled.js';
-import Nav from '../../components/Nav';
-
-
+const ListGroup = styled.li`
+`
+const Reservation = styled.div`
+`
+const CustomerQueue = styled.div`
+`
 class Queue extends Component {
   state = {
     queue: [],
@@ -27,11 +65,9 @@ class Queue extends Component {
     notes: '',
     queuePosition: ''
   };
-
   componentDidMount() {
     this.loadQueue();
   }
-
   loadQueue = () => {
     API.getQueue()
       .then(res =>
@@ -50,20 +86,17 @@ class Queue extends Component {
       )
       .catch(err => console.log(err));
   };
-
-  removeGuestFromQueue = id => {
-    API.removeGuestFromQueue(id)
+  deleteGuest = id => {
+    API.deleteGuest(id)
       .then(res => this.loadQueue())
       .catch(err => console.log(err));
   };
-
   handleInputChange = event => {
     const { name, value } = event.target;
     this.setState({
       [name]: value
     });
   };
-
   handleFormSubmit = event => {
     event.preventDefault();
     console.log(event);
@@ -85,11 +118,8 @@ class Queue extends Component {
         .catch(err => console.log(err));
     }
   };
-
-
   render() {
     return (
-
       <Container>
           <h2 className="text-center host-head">Reserve a Table</h2>
             <Input
@@ -150,7 +180,7 @@ class Queue extends Component {
                     </strong>
                     <Button
                       onClick={() => {
-                        this.removeGuestFromQueue(guest._id);
+                        this.deleteGuest(guest._id);
                       }}
                     />
                   </List>
@@ -164,17 +194,82 @@ class Queue extends Component {
     );
   }*/
 
-
 class Queue extends Component {
+  state = {
+    queue: [],
+    firstName: '',
+    lastName: '',
+    phoneNumber: '',
+    partySize: '',
+    notes: '',
+    queuePosition: ''
+  };
+  componentDidMount() {
+    this.loadQueue();
+  }
+  loadQueue = () => {
+    API.getQueue()
+      .then(res =>
+        this.setState({
+          queue: res.data,
+          firstName: '',
+          lastName: '',
+          phoneNumber: '',
+          notes: '',
+          partySize: '',
+          seated: Boolean,
+          moveUp: Boolean,
+          moveDown: Boolean,
+          queuePosition: ''
+        })
+      )
+      .catch(err => console.log(err));
+  };
+  deleteGuest = id => {
+    API.deleteGuest(id)
+      .then(res => this.loadQueue())
+      .catch(err => console.log(err));
+  };
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+  handleFormSubmit = event => {
+    event.preventDefault();
+    console.log(event);
+    if (
+      this.state.firstName &&
+      this.state.phoneNumber &&
+      this.state.partySize
+    ) {
+      console.log('first request');
+      API.saveGuest({
+        firstName: this.state.firstName,
+        lastName: this.state.lastName,
+        phoneNumber: this.state.phoneNumber,
+        partySize: this.state.partySize,
+        notes: this.state.notes,
+        queuePosition: this.state.queue.length + 1
+      })
+        .then(res => this.loadQueue())
+        .catch(err => console.log(err));
+    }
+  };
   render() {
     return (
-      <Container>
+      <Wrapper>
+        <Container1>
           <Counter />
-          <NewReservation />
+          <NewReservation onClick={this.handleFormSubmit} />
           <Icons />
+        </Container1>
+        <Container2>
           <CustomerQueue />
-      </Container>
-    )
+        </Container2>
+      </Wrapper>
+    );
   }
 }
 
