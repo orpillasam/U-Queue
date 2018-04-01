84028d6de0d014eb12a28b051bd7b61a5c415c2b
orpillasam <orpillasam@gmail.com>
2018-04-01 11:58:32 -0700

    twilio function added

---
+++ apikeys/apikeys.js
@@ -0,0 +1,8 @@
+
+const apiKeys = {
+    accountSid: 'AC2857cf2eea07fbef8e9aa03560e2aade',
+    authToken: 'dbb0757d4ca2f8495e5ed9171f7af836',
+    myPhoneNumber: '+19495369535'
+};
+
+module.exports = apiKeys;
